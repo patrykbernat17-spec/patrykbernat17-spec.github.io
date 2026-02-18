804a075a91ead5ae383787f47b80acd2e2f2a6f3
patrykbernat17-spec <patrykbernat17@gmail.com>
2026-02-18 02:32:40 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      {/* DODANO basename, aby router wiedział, że jest w podkatalogu */}
-      <BrowserRouter basename="/fluxsite.pl">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
